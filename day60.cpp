887a6cbfd82c8a0642b6c0170140fa7c1f3078d3
anshd258 <anshd268@gmail.com>
2020-12-19 00:52:30 +0530

    used stack to create a good  working model for day60.cpp

--- day60.cpp
+++ day60.cpp
@@ -1,38 +1,32 @@
 #include<iostream>
 #include<string>
+#include<stack>
 using namespace std;
 bool cool(string &misaka)
 {
+    stack<char> clues;
     int count=0,i=0;
-    try{
+    try{if(misaka.length()%2!=0)
+        throw 303;
    while(i<misaka.length())
    {
-       switch (misaka[i])
-       {
-       case '{':count++;
-                break;
-       case '[':count++;
-                break;
-        case '(':count++;
-                 break;
-        case '}':count--;
-                 break;
-        case ']':count--;
-                 break;
-        case ')':count--;
-                 break;                                       
-       
-       default: throw 404;
-       }
-       ++i;
+     if(misaka[i]=='{'||misaka[i]=='['||misaka[i]=='(')
+     {
+         clues.push(misaka[i]);
+     }
+     if((misaka[i]=='}'&&clues.top()=='{')||(misaka[i]==']'&&clues.top()=='[')||(misaka[i]==')'&&clues.top()=='('))
+     clues.pop();
+     ++i;
    }
     }
-    catch(int l)
+      catch(int l)
     {
         cout<<"wrong input the statement should not consist of any other elemnet other than prantheses error "<<l<<endl;
         exit(0);
     }
-      if(count==0)
+   
+   
+      if(clues.size()==0)
    return true;
 return false;
 }
